250e618d450835712f8076cd56870a8a1322082a
hayleystevens <hayley.stevens@slalom.com>
2019-02-11 16:37:14 -0800

    updated - test

--- embedded_media.js
+++ embedded_media.js
@@ -58,65 +58,34 @@
 
     return button;
   }
+  
+function listenToMarksSelection() {
+  viz.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
+}
 
-  function loadSelectedMarks (worksheetName) {
-    // Get the worksheet object we want to get the selected marks for
-    const worksheet = getSelectedSheet(worksheetName);
+function onMarksSelection(marksEvent) {
+  return marksEvent.getMarksAsync().then(reportSelectedMarks);
+}
 
-    // Set our title to an appropriate value
-    $('#selected_marks_title').text(worksheet.name);
+function reportSelectedMarks(marks) {
+  var html = "";
 
-    // Call to get the selected marks for our sheet
-    worksheet.getSelectedMarksAsync().then(function (marks) {
-      // Get the first DataTable for our selected marks (usually there is just one)
-      const worksheetData = marks.data[0];
+  for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+      var pairs = marks[markIndex].getPairs();
+      html += "<b>Mark " + markIndex + ":</b><ul>";
 
-      // Map our data into the format which the data table component expects it
-      const data = worksheetData.data.map(function (row, index) {
-        const rowData = row.map(function (cell) {
-          return cell.formattedValue;
-        });
+      for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+          var pair = pairs[pairIndex];
+          html += "<li><b>Field Name:</b> " + pair.fieldName;
+          html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
+      }
 
-        return rowData;
-      });
-
-      const columns = worksheetData.columns.map(function (column) {
-        return { title: column.fieldName };
-      });
-
-      // Populate the data table with the rows and columns we just pulled out
-      populateDataTable(data, columns);
-    });
+      html += "</ul>";
   }
 
-  function populateDataTable (data, columns) {
-    // Do some UI setup here to change the visible section and reinitialize the table
-    $('#data_table_wrapper').empty();
-
-    if (data.length > 0) {
-      $('#no_data_message').css('display', 'none');
-      $('#data_table_wrapper').append(`<table id='data_table' class='table table-striped table-bordered'></table>`);
-
-      // Do some math to compute the height we want the data table to be
-      var top = $('#data_table_wrapper')[0].getBoundingClientRect().top;
-      var height = $(document).height() - top - 130;
-
-      // Initialize our data table with what we just gathered
-      $('#data_table').DataTable({
-        data: data,
-        columns: columns,
-        autoWidth: false,
-        deferRender: true,
-        scroller: true,
-        scrollY: height,
-        scrollX: true,
-        dom: "<'row'<'col-sm-6'i><'col-sm-6'f>><'row'<'col-sm-12'tr>>" // Do some custom styling
-      });
-    } else {
-      // If we didn't get any rows back, there must be no marks selected
-      $('#no_data_message').css('display', 'inline');
-    }
-  }
+  var infoDiv = document.getElementById('markDetails');
+  infoDiv.innerHTML = html;
+}
 
   function initializeButtons () {
     $('#show_choose_sheet_button').click(showChooseSheetDialog);
