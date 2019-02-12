17eff5bdbec2d7b564d48d41675279dfba216461
hayleystevens <hayley.stevens@slalom.com>
2019-02-11 22:59:01 -0800

    update

--- embedded_media.js
+++ embedded_media.js
@@ -6,48 +6,43 @@ $(document).ready(function() {
 
     // Disable the button after it's been clicked
     $("#initializeButton").prop('disabled', true);
+    // The first step in choosing a sheet will be asking Tableau what sheets are available
+    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
     var sheetname="Highlight Table"
 
     tableau.extensions.initializeAsync().then(function() {
-
+      
       // Initialization succeeded! Get the dashboard
       var dashboard = tableau.extensions.dashboardContent.dashboard;
-      $("#SheetName").text(sheetname.value)
+      $("#SheetName").text(worksheets)
 
       function listenToMarksSelection() {
         sheetname.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
-    }
+      }
 
-    function onMarksSelection(marksEvent) {
+      function onMarksSelection(marksEvent) {
         return marksEvent.getMarksAsync().then(reportSelectedMarks);
-    }
+      }
 
-    function reportSelectedMarks(marks) {
-       
-      for (var markIndex = 0; markIndex < marks.length; markIndex++) {
-            var pairs = marks[markIndex].getPairs();
-            html += "<b>Mark " + markIndex + ":</b><ul>";
+      function reportSelectedMarks(marks) {
+        for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+          var pairs = marks[markIndex].getPairs();
+          html += "<b>Mark " + markIndex + ":</b><ul>";
 
-            for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
-                var pair = pairs[pairIndex];
-                html += "<li><b>Field Name:</b> " + pair.fieldName;
-                html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
-            }
-
-            html += "</ul>";
+          for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+            var pair = pairs[pairIndex];
+            html += "<li><b>Field Name:</b> " + pair.fieldName;
+            html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
+          }
+          html += "</ul>";
         }
-
-     }
-
+      }
       // Display the name of dashboard in the UI
       $("#resultBox").html("This dashboard is called " + dashboard.name+". The sheet is called" +sheetname + markDetails);
     }, function(err) {
-
       // something went wrong in initialization
       $("#resultBox").html("Error while Initializing: " + err.toString());
       $("#AllMarks").html(markDetails);
-
     });
-
   });
 });
