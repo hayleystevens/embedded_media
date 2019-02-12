13aca3ddb24d98af0d01b026bfc9e7b0323711fe
hayleystevens <hayley.stevens@slalom.com>
2019-02-11 21:53:59 -0800

    update

--- embedded_media.js
+++ embedded_media.js
@@ -14,12 +14,42 @@ $(document).ready(function() {
       var dashboard = tableau.extensions.dashboardContent.dashboard;
       $("#SheetName").text(sheetname)
 
+      function listenToMarksSelection() {
+        sheetname.addEventListener(tableau.TableauEventName.MARKS_SELECTION, onMarksSelection);
+    }
+
+    function onMarksSelection(marksEvent) {
+        return marksEvent.getMarksAsync().then(reportSelectedMarks);
+    }
+
+    function reportSelectedMarks(marks) {
+        var html = "";
+
+        for (var markIndex = 0; markIndex < marks.length; markIndex++) {
+            var pairs = marks[markIndex].getPairs();
+            html += "<b>Mark " + markIndex + ":</b><ul>";
+
+            for (var pairIndex = 0; pairIndex < pairs.length; pairIndex++) {
+                var pair = pairs[pairIndex];
+                html += "<li><b>Field Name:</b> " + pair.fieldName;
+                html += "<br/><b>Value:</b> " + pair.formattedValue + "</li>";
+            }
+
+            html += "</ul>";
+        }
+
+        var infoDiv = document.getElementById('markDetails');
+        infoDiv.innerHTML = html;
+    }
+
       // Display the name of dashboard in the UI
       $("#resultBox").html("This dashboard is called " + dashboard.name+". The sheet is called" +sheetname);
     }, function(err) {
 
       // something went wrong in initialization
       $("#resultBox").html("Error while Initializing: " + err.toString());
+
     });
+
   });
 });
